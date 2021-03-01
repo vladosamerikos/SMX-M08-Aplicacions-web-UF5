b44431044ea33a2eca4e19737309dee35a357b3b
vladosamerikos <73399989+vladosamerikos@users.noreply.github.com>
2021-03-01 19:16:48 +0100

    Add files via upload
    JS

---
+++ js/act42_Vladyslav_Pasichnyk/js/script.js
@@ -0,0 +1,11 @@
+function canvia(elem) {
+   elem.style.backgroundImage = 'url(./img/llumon.gif)';
+   }
+
+function desfe(elem) {
+    elem.style.backgroundImage = 'url(./img/llumoff.gif)';
+}
+
+function destroy(elem) {
+    elem.style.backgroundImage = 'url(./img/llumbreak.gif)';
+}
